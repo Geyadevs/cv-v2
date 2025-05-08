647a9d958bf7eae26c9d62d337318bae87360b89
Geyadevs <geyadevs@gmail.com>
2025-05-08 15:28:03 +0800

    Nav

--- cv-v2/src/components/Navbar.jsx
+++ cv-v2/src/components/Navbar.jsx
@@ -3,13 +3,13 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <nav className="navbar bg-gray-800 text-white p-4 mb-10">
-      <ul className="flex space-x-4">
+    <nav className="navbar bg-gray-100 text-white p-4 mb-10">
+      <ul className="flex flex-col md:flex-row md:space-x-12 justify-center items-center">
         <li>
-          <Link to="/" className="hover:underline">Home</Link>
+          <Link to="/" className="hover:underline text-gray-800">Home</Link>
         </li>
         <li>
-          <Link to="/about" className="hover:underline">About</Link>
+          <Link to="/about" className="hover:underline text-gray-800">About</Link>
         </li>
       </ul>
     </nav>
